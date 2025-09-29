03c119744054a0686d89ec8461816ea9d5e298ce
Xyren <95626487+XyrenTheCoder@users.noreply.github.com>
2025-09-29 20:08:37 +0800

    require bypass

--- gradient-and-font.js
+++ gradient-and-font.js
@@ -2,7 +2,7 @@
 // @name         A A A Diep.io Gradient Name, Change Font
 // @version      0.1
 // @description  Gradient name and font in one script
-// @author       shlong#2873, Elijaah & Acc1, Edited and Merged by A-76, Solar
+// @author       shlong#2873, Elijaah & Acc1, Edited and Merged by A-76
 // @match        *://diep.io/*
 // @run-at       document-start
 // @grant        unsafeWindow
@@ -64,7 +64,7 @@
     });
 
 
-    
+
     const fontLink = document.createElement("link");
     fontLink.rel = "stylesheet";
     fontLink.href = "https://fonts.googleapis.com/css2?family=Diphylleia&display=swap";
@@ -76,74 +76,11 @@
         }
     `);
 
-    let a;
-    class _a {
-        constructor() {
-            this.p = {};
-            this.v = false;
-            document.addEventListener("DOMContentLoaded", this.tu.bind(this));
+    const { set: originalFontSetter } = Object.getOwnPropertyDescriptor(CanvasRenderingContext2D.prototype, "font");
+    Object.defineProperty(CanvasRenderingContext2D.prototype, "font", {
+        set(value) {
+            const newValue = value.replace(/Ubuntu|sans-serif/g, 'Diphylleia');
+            originalFontSetter.call(this, newValue);
         }
-        tu() {
-            if (this.v) return;
-            this.v = true;
-            const ogGetElementById = HTMLDocument.prototype.getElementById;
-            HTMLDocument.prototype.getElementById = function (id) {
-                const elem = ogGetElementById.call(document, id);
-                if (id === "canvas") return wrapCanvas(elem);
-                return elem;
-            };
-            const ogCreateElement = HTMLDocument.prototype.createElement;
-            HTMLDocument.prototype.createElement = function (tag) {
-                const elem = ogCreateElement.call(document, tag);
-                if (tag === "canvas") return wrapCanvas(elem);
-                return elem;
-            };
-            function wrapCanvas(origCanvas) {
-                class HTMLCanvasElementProxy {}
-                let proxyCanvas = new HTMLCanvasElementProxy();
-                proxyCanvas.width = origCanvas.width;
-                proxyCanvas.height = origCanvas.height;
-                proxyCanvas.transferControlToOffscreen = origCanvas.transferControlToOffscreen?.bind(origCanvas);
-                proxyCanvas.toDataURL = origCanvas.toDataURL?.bind(origCanvas);
-                proxyCanvas.toBlob = origCanvas.toBlob?.bind(origCanvas);
-                proxyCanvas.captureStream = origCanvas.captureStream?.bind(origCanvas);
-                proxyCanvas.getContext = function (...args) {
-                    let ctx = origCanvas.getContext(...args);
-                    if (args[0] !== "2d") return ctx;
-                    return new Proxy(ctx, {
-                        get(target, prop) {
-                            const original = target[prop];
-                            if (typeof original !== "function") return original;
-                            if (Object.keys(a.p).includes(prop)) {
-                                return function (...pArgs) {
-                                    let skip = false;
-                                    a.p[prop].forEach((hook) => {
-                                        const result = hook(ctx, ...pArgs);
-                                        if (!result) skip = true;
-                                        else [ctx, pArgs] = result;
-                                    });
-                                    if (skip) return;
-                                    return original.apply(target, pArgs);
-                                };
-                            }
-                            return original.bind(target);
-                        },
-                        set(target, prop, value) {
-                            if (prop === "font" && typeof value === "string") {
-                                value = value.replace(/Ubuntu|sans-serif/gi, "'Diphylleia'");
-                            }
-                            target[prop] = value;
-                            return true;
-                        },
-                    });
-                };
-                return proxyCanvas;
-            }
-        }
-        _(methodName, hookFn) {
-            if (Object.keys(this.p).includes(methodName)) this.p[methodName].push(hookFn);
-            else this.p[methodName] = [hookFn];
-        }
-    }
-    a = new _a();
+    });
 })();
